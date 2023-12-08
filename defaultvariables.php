dd8dafe21c64d742479f38c542dd702163558eb2
Ma. Francezca L. Ramos <131897056+mafranzramos@users.noreply.github.com>
2023-12-08 12:24:25 +0800

    added our free mapbox access token for easier checking

--- defaultvariables.php
+++ defaultvariables.php
@@ -1,5 +1,5 @@
 <?php 
-    $mapboxAccessToken = "";
+    $mapboxAccessToken = "pk.eyJ1IjoiY3p5bm9uam9obiIsImEiOiJjbG9xZWVzcnIwaDBpMmttenpza2I1ajZqIn0.SXmiSmtjjBMSmMA_rmVwiw";
     $servername = "";
     $username = "";
     $password = "";
